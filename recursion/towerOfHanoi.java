a4431fbc53f40a471cccaebd341fd772bec74662
dipishBisht <dipish1718@gmail.com>
2025-03-20 10:06:22 +0530

    updated code

--- recursion/towerOfHanoi.java
+++ recursion/towerOfHanoi.java
@@ -11,6 +11,7 @@ public class towerOfHanoi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // Take inputs
         System.out.println("Enter no of disks :");
         int disks = sc.nextInt();
         System.out.println("Enter tower 1 name :");
@@ -20,13 +21,18 @@ public class towerOfHanoi {
         System.out.println("Enter tower 3 name :");
         String t3 = sc.next();
 
+        sc.close();
+
+        // Print instructions
         printInstructions(disks, t1, t2, t3);
     }
 
     public static void printInstructions(int disks, String t1, String t2, String t3) {
+        // Base case
         if (disks == 0)
             return;
 
+        // Do your work and divide into sub problem
         printInstructions(disks - 1, t1, t3, t2);
         System.out.println(disks + " from " + t1 + " to " + t2);
         printInstructions(disks - 1, t3, t2, t1);
